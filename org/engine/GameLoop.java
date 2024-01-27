388a39168f2f75ca1a13f7f57672a5f67ab1b469
Crispen LeBlanc <leblanc2042@gmail.com>
2024-01-27 14:07:24 -0600

    Make game world nanoTime and FPS Count

--- org/engine/GameLoop.java
+++ org/engine/GameLoop.java
@@ -7,19 +7,62 @@ public class GameLoop {
 	
 	private static boolean running = false;
 
+	private static int updates = 0;
+	private static final int MAX_UPDATES = 5;
+	private static long lastUpdateTime =0;
+	// 
 	private static int targetFPS = 60;
-	
 	// Number of 
 	private static int targetTime = 1000000000 / targetFPS;
 
 	public static void start() {
 		Thread thread = new Thread() {
-		
 			public void run( ) {
 			
 				running = true;
+
+				// Set systemTime to the game System time instead of the host system
+				lastUpdateTime = System.nanoTime();
+
+				int fps = 0;
+				long lastFPSCheck = System.nanoTime();
 				
 				while (running) {
+
+					long currentTime = System.nanoTime();
+
+					updates = 0;
+
+					while(currentTime - lastUpdateTime >= targetTime) {
+						World.update();
+						lastUpdateTime += targetTime;
+						updates ++;
+
+						if (updates > MAX_UPDATES) {
+							break;
+						}
+					}
+
+					Renderer.renderer();
+
+					fps++;
+					if(System.nanoTime() >= lastFPSCheck + 1000000000){
+						System.out.println(fps);
+						fps = 0;
+						lastFPSCheck = System.nanoTime();
+					}
+					
+					long timeTaken = System.nanoTime() - currentTime;
+					if (targetTime > timeTaken) {
+						try {
+							Thread.sleep((targetTime - timeTaken) / 1000000);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+
+
+					/* 
 					long startTime = System.nanoTime();
 
 					// Poll Input
@@ -38,7 +81,7 @@ public class GameLoop {
 						} catch (InterruptedException e) {
 							e.printStackTrace();
 						}
-					}
+					} */
 				}
 			}
 		
